660d40bdb02c5dfe33e375736131b451c27a6e08
Alex <a.schaaf@abdn.ac.uk>
2019-05-10 10:00:04 +0100

    update requirements pytest

--- tests/test_seismic.py
+++ tests/test_seismic.py
@@ -4,5 +4,6 @@ from subsurface import Seismic
 
 
 def test_from_segy():
+    """Test creating Seismic instance from SEGY file."""
     seismic = from_segy("tests/data/test.segy")
     assert type(seismic) == Seismic
