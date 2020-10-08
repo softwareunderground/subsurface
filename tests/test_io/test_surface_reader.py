492d44664cffd9dde67d69d51081eda2a8acc43f
MonEstCha <monique.charrier@cim.rwth-aachen.de>
2020-10-08 15:30:48 +0200

    tests have been corrected and more added to them

--- tests/test_io/test_surface_reader.py
+++ tests/test_io/test_surface_reader.py
@@ -32,25 +32,29 @@ def test_return_type(get_unstructured_data):
 
 def test_dataframes(get_unstructured_data):
     assert len(get_unstructured_data.vertex) == 132695
-    assert len(get_unstructured_data.edges) == 863814
+    assert len(get_unstructured_data.edges) == 263894
 
 
 def test_edges_shape(get_unstructured_data):
-    assert get_unstructured_data.edges.shape[1] == 4
+    print(get_unstructured_data.vertex)
+    assert get_unstructured_data.edges.shape[1] == 3
 
 
 def test_unstructured_element(get_less_unstructured_data):
-    tm = TetraMesh(get_less_unstructured_data)
-    assert len(tm.tetrahedrals) == 39
-    assert isinstance(tm, TetraMesh)
+    ts = TriSurf(get_less_unstructured_data)
+    assert len(ts.triangles) == 17
+    assert isinstance(ts, TriSurf)
 
 
-def test_plot_pyvista(get_less_unstructured_data):
+def test_plot_pyvista(get_unstructured_data):
+    ts = TriSurf(get_unstructured_data) # The element type should be TriSurf
+    s = to_pyvista_mesh(ts) # Process finished here with exit code 139 (interrupted by signal 11: SIGSEGV)
+    # assert isinstance(s, UnstructuredGrid) This assert is wrong
+    pv_plot([s], image_2d=True)
+
+
+def test_plot_less_pyvista(get_less_unstructured_data):
     ts = TriSurf(get_less_unstructured_data) # The element type should be TriSurf
     s = to_pyvista_mesh(ts) # Process finished here with exit code 139 (interrupted by signal 11: SIGSEGV)
     # assert isinstance(s, UnstructuredGrid) This assert is wrong
     pv_plot([s], image_2d=True)
-#
-# def test_land_surface_vertices_coords(get_vertices):
-#     assert get_vertices.iloc[5][2] == 1493.90209961
-#     assert get_vertices.iloc[-1][-1] == 2430.35742188
