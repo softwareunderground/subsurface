bd6bff8042f1058868337fb2aa48adc2ef76216f
MonEstCha <monique.charrier@cim.rwth-aachen.de>
2020-10-27 15:35:15 +0100

    First attempt to plot in pyvista but only failing the test

--- tests/test_geological_formats/test_segy_reader.py
+++ tests/test_geological_formats/test_segy_reader.py
@@ -26,11 +26,10 @@ def test_converted_to_structured_data(get_structured_data):
 
 
 def test_pyvista_structured_grid(get_structured_data):
-
+    from subsurface.visualization import to_pyvista_grid
+    from subsurface.visualization import pv_plot
     # StructuredData from Dataset
     for x in get_structured_data:
         sg = StructuredGrid(x)
-    from subsurface.visualization import to_pyvista_grid
-    s = to_pyvista_grid(sg, 'data') # works only for test.segy as x.shape == y.shape so the test fails here
-    from subsurface.visualization import pv_plot
-    pv_plot([s], image_2d=True)
+        s = to_pyvista_grid(sg, 'data') # works only for test.segy as x.shape == y.shape so the test fails here
+        pv_plot([s], image_2d=True)
