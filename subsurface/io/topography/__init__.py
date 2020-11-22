b28fdf87672ff3f5860412863d6a209320435c99
Leguark <varga@cgre.rwth-aachen.de>
2020-11-22 10:04:59 +0100

    - [CLN] refactor surface_reader.py

--- subsurface/io/topography/__init__.py
+++ subsurface/io/topography/__init__.py
@@ -4,6 +4,7 @@ import numpy as np
 from scipy.spatial.qhull import Delaunay
 
 from subsurface import UnstructuredData, StructuredData
+from subsurface.io.mesh.surface_reader import read_2d_mesh
 from subsurface.utils import get_extension
 
 
@@ -39,28 +40,10 @@ def rasterio_dataset_to_structured_data(dataset):
 
 
 def read_unstructured_topography(path) -> UnstructuredData:
-    extension = get_extension(path)
-    if extension == '.dxf':
-        faces, vertex = dxf_to_vertex_edges(path)
-    else:
-        raise NotImplementedError('The extension given cannot be read yet')
-    unstruct = UnstructuredData(vertex, faces)
-    return unstruct
+    return read_2d_mesh(path)
 
 
-def dxf_to_vertex_edges(path):
-    dataset = ezdxf.readfile(path)
-    vertex = []
-    entity = dataset.modelspace()
-    for e in entity:
-        vertex.append(e[0])
-        vertex.append(e[1])
-        vertex.append(e[2])
-    vertex = np.array(vertex)
-    vertex = np.unique(vertex, axis=0)
-    tri = Delaunay(vertex[:, [0, 1]])
-    faces = tri.simplices
-    return faces, vertex
+
 
 
 
