631538ddb44235aac1f8f07f8ce404657e75c576
prisae <prisae@users.noreply.github.com>
2021-04-19 19:44:22 +0000

    Publish 'docs/build/html' to 'gh-pages' in 'softwareunderground/subsurface'

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.0b2',
+    VERSION: '0.1.dev1+g2e5bd0e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
