d4eabc65c1f899b09ac18d5e0b2ea512ccccdfa8
Leguark <Leguark@users.noreply.github.com>
2021-05-06 08:56:24 +0000

    Publish 'docs/build/html' to 'gh-pages' in 'softwareunderground/subsurface'

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.5.dev30+ga9b7636',
+    VERSION: '0.2.5.dev31+g4406527',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
