956850834849a334d96c176d004dae828da1e439
Leguark <leguiom@gmail.com>
2021-11-12 09:39:47 +0100

    - [BUG] Narrowing error again

--- examples/examples/gempy_model.py
+++ examples/examples/gempy_model.py
@@ -22,17 +22,17 @@ model_files = pooch.retrieve(
     processor=pooch.Unzip()
 )
 
-# # %%
-# fname, = [i for i in model_files if "meshes.nc" in i]
-# dataset = ss.reader.read_netcdf.read_unstruct(fname)
-#
-# # %%
-# obj = ss.TriSurf(dataset)
-# print(obj.mesh.points_attributes_to_dict)
-#
-# mesh = ss.visualization.to_pyvista_mesh(obj)
-# ss.visualization.pv_plot([mesh])
-#
+# %%
+fname, = [i for i in model_files if "meshes.nc" in i]
+dataset = ss.reader.read_netcdf.read_unstruct(fname)
+
+# %%
+obj = ss.TriSurf(dataset)
+print(obj.mesh.points_attributes_to_dict)
+
+mesh = ss.visualization.to_pyvista_mesh(obj)
+ss.visualization.pv_plot([mesh])
+
 #
 # # %% md
 # # Four Layers
