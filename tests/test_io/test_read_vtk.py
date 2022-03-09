fa545ccb110a9db27b13378e52c8c4ffd25af064
Leguark <miguel@terranigma-solutions.com>
2022-03-09 14:50:26 +0100

    - [TEST] Add tests for vtk and las

---
+++ tests/test_io/test_read_vtk.py
@@ -0,0 +1,26 @@
+﻿import numpy as np
+import pyvista as pv
+from pyvista import UnstructuredGrid
+from scipy.interpolate import griddata
+
+
+def test_read_vtk():
+    address = r"C:\Users\legui\OneDrive - Terranigma Solutions GmbH\Documents\Products and Services\LiquidEarth\Test Data Sets\ET\DeepERT_Hombourg.vtk"
+    mesh: UnstructuredGrid = pv.read(address)
+    point_data_mesh = mesh.cell_data_to_point_data()
+
+    vertex = np.array(point_data_mesh.points)
+    cell = np.array(point_data_mesh.cells)
+
+    attributes_names = point_data_mesh.array_names
+    point_arrays = point_data_mesh.point_arrays
+
+    bounds = mesh.bounds
+    x = np.linspace(bounds[0], bounds[1], 100)
+    y = np.linspace(bounds[2], bounds[3], 100)
+    z = np.linspace(bounds[4], bounds[5], 100)
+    xg, yg, zg = np.meshgrid(x, y, z, indexing='ij', sparse=False)
+    point_array = point_arrays[attributes_names[0]]
+    grid_z2 = griddata(vertex, point_array, (xg, yg, zg), method='nearest')
+
+    mesh.plot()
---
+++ tests/test_io/test_read_wells_from_las.py
@@ -0,0 +1,21 @@
+﻿import subsurface
+from subsurface import LineSet
+
+from subsurface.reader.wells import WellyToSubsurfaceHelper, welly_to_subsurface
+
+
+# TODO: Rename to test_read_from_las. I leave it like this to avoid calling it with the github actions. 
+def _read_from_las():
+    # There reading the code from las to welly
+    # ...
+
+    wts = WellyToSubsurfaceHelper()
+    # wts.p += eachWell
+
+    unstructured_data = welly_to_subsurface(wts)
+    print('\n', unstructured_data)
+    element = LineSet(unstructured_data)
+    pyvista_mesh = subsurface.visualization.to_pyvista_line(element)
+
+    # Plot default LITH
+    subsurface.visualization.pv_plot([pyvista_mesh], image_2d=True)
