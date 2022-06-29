120b39ac1c7f00a46e1ac4686d6dddc0296fbdf4
Jan Niederau <jan.niederau@rwth-aachen.de>
2022-06-29 16:56:50 +0200

    [DOC] remove todo

--- subsurface/reader/readers_data.py
+++ subsurface/reader/readers_data.py
@@ -23,7 +23,7 @@ elif pd.__version__ >= '1.4.0':
 
 @dataclass
 class ReaderFilesHelper:
-    file_or_buffer: fb # TODO: Change FilePath and other private types and methods of pandas to own ones
+    file_or_buffer: fb 
 
 
     usecols: Union[List[str], List[int]] = None # Use a subset of columns
