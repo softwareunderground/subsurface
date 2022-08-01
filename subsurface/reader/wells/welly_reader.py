95ff425eec28afa7f2fe07351539021f08d4ee77
Jan Niederau <jan.niederau@rwth-aachen.de>
2022-08-01 14:14:29 +0200

    [BUG] look for datum of borehole

--- subsurface/reader/wells/welly_reader.py
+++ subsurface/reader/wells/welly_reader.py
@@ -97,7 +97,12 @@ def vertex_and_cells_from_welly_trajectory(cells: np.ndarray, elev: bool,
                                            welly_trajectory_kwargs: dict,
                                            last_index: int, n_vertex_for_well: int,
                                            vertex: np.ndarray, w: Well):
-    xyz = w.location.trajectory(datum=None, elev, n_vertex_for_well, **welly_trajectory_kwargs) #w.location.position
+    try:
+        datum = w.location.datum
+    except AttributeError:
+        datum = None
+
+    xyz = w.location.trajectory(datum=datum, elev, n_vertex_for_well, **welly_trajectory_kwargs) #w.location.position
     # Make sure deviation is there
     a = np.arange(0 + last_index, xyz.shape[0] - 1 + last_index, dtype=np.int_)
     b = np.arange(1 + last_index, xyz.shape[0] + last_index, dtype=np.int_)
