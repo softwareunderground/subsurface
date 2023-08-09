a9da80110e5d6abb441eff5a75f08dd46e874e12
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-08-09 13:52:52 +0200

    [ENH] Added optional_requirements concept

---
+++ subsurface/optional_dependencies.py
@@ -0,0 +1,6 @@
+﻿def require_omf():
+    try:
+        import omfvista
+    except ImportError:
+        raise ImportError("The omfvista package is required to run this function.")
+    return omfvista
