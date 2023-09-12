cc3adabd9e7842ef2604c21371bbd7f86fe448a5
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-09-12 09:49:50 +0200

    - [WIP] Loading topography

--- subsurface/optional_dependencies.py
+++ subsurface/optional_dependencies.py
@@ -4,3 +4,11 @@
     except ImportError:
         raise ImportError("The omfvista package is required to run this function.")
     return omfvista
+
+
+def require_rasterio():
+    try:
+        import rasterio
+    except ImportError:
+        raise ImportError("The rasterio package is required to run this function.")
+    return rasterio
