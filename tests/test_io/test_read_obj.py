45ff15702175ab5555af21e0673e236fc3fa845f
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-09-13 15:16:32 +0200

    - [WIP] Added some code to import obj using pyvista. Textures are not in the right place

---
+++ tests/test_io/test_read_obj.py
@@ -0,0 +1,19 @@
+﻿import pyvista as pv
+
+from subsurface.reader.mesh.obj_reader import plot_obj_with_multiple_textures
+
+
+def test_read_obj():
+    path_to_obj = ""
+    reader = pv.get_reader(path_to_obj)
+    mesh = reader.read()
+    mesh.plot()
+    
+    
+def test_read_obj_with_texture():
+    path_to_obj = ""
+    path_to_mtl = ""
+    plot_obj_with_multiple_textures(
+        obj_path=path_to_obj,
+        mtl_path=path_to_mtl
+    )
