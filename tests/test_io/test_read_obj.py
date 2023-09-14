fe6220c7c1c377aa1cd726848fd0d1e2972d2e99
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-09-14 08:13:58 +0200

    - [META] update versions

--- tests/test_io/test_read_obj.py
+++ tests/test_io/test_read_obj.py
@@ -4,15 +4,15 @@ from subsurface.reader.mesh.obj_reader import plot_obj_with_multiple_textures
 
 
 def test_read_obj():
-    path_to_obj = ""
+    path_to_obj = "/mnt/d/OneDrive - Terranigma Solutions GmbH/Documents/Projects/VGC/Models/Broadhaven_Obj/model/model.obj"
     reader = pv.get_reader(path_to_obj)
     mesh = reader.read()
     mesh.plot()
     
     
 def test_read_obj_with_texture():
-    path_to_obj = ""
-    path_to_mtl = ""
+    path_to_obj = "/mnt/d/OneDrive - Terranigma Solutions GmbH/Documents/Projects/VGC/Models/Broadhaven_Obj/model/model.obj"
+    path_to_mtl = "/mnt/d/OneDrive - Terranigma Solutions GmbH/Documents/Projects/VGC/Models/Broadhaven_Obj/model/model.mtl"
     plot_obj_with_multiple_textures(
         obj_path=path_to_obj,
         mtl_path=path_to_mtl
