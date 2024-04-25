fef7ef2d923c78c30c43348bd55787cef2981b8d
MigueldelaVarga <miguel@terranigma-solutions.com>
2024-04-25 12:48:44 +0200

    [ENH] Update setup.py to dynamically read requirements files
    A function is added to setup.py that reads the project requirements from different files dynamically. This relevant addition allows for greater flexibility when handling dependencies. Additionally, the requirements files have been renamed and moved to a dedicated folder to enhance project organization.

--- setup.py
+++ setup.py
@@ -18,12 +18,32 @@ with open(readme, "r", encoding="utf-8") as f:
 long_description = long_description.split('inclusion-marker')[-1]
 
 CLASSIFIERS = [
-    "Development Status :: 3 - Alpha",
-    "Intended Audience :: Science/Research",
-    "Natural Language :: English",
-    "License :: OSI Approved :: Apache Software License",
-    "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
 ]
+
+
+def read_requirements(file_name):
+    requirements = []
+    with open(file_name, "r", encoding="utf-8") as f:
+        for line in f:
+            # Strip whitespace and ignore comments
+            line = line.strip()
+            if line.startswith("#") or not line:
+                continue
+
+            # Handle -r directive
+            if line.startswith("-r "):
+                referenced_file = line.split()[1]  # Extract the file name
+                requirements.extend(read_requirements(referenced_file))  # Recursively read referenced file
+            else:
+                requirements.append(line)
+
+    return requirements
+
 
 setup(
     name="subsurface",
@@ -35,13 +55,18 @@ setup(
     author="Software Underground",
     author_email="hello@softwareunderground.org",
     license="Apache-2.0",
-    install_requires=["xarray", ],
+    install_requires=read_requirements("requirements/requirements.txt"),
+    extras_require={
+            "opt"           : read_requirements("requirements/optional-requirements.txt"),
+            "opt-geospatial": read_requirements("requirements/requirements_opt_geospatial.txt"),
+            "dev"           : read_requirements("requirements/requirements_dev.txt"),
+    },
     classifiers=CLASSIFIERS,
     zip_safe=False,
     use_scm_version={
-        "root": ".",
-        "relative_to": __file__,
-        "write_to": path.join("subsurface", "_version.py"),
+            "root"       : ".",
+            "relative_to": __file__,
+            "write_to"   : path.join("subsurface", "_version.py"),
     },
     setup_requires=["setuptools_scm"],
 )
