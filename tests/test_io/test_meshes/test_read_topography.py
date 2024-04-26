fb34deb802cbac8c78fd874bbf40f9eb05bffe4c
MigueldelaVarga <miguel@terranigma-solutions.com>
2024-04-26 14:01:25 +0200

    [TEST] Add skip conditions to topography tests
    In the test_read_topography.py file, pytest.mark.skipif conditions have been added to the test_read_topography_from_dxf and test_read_topography_from_tif tests to specify requirement levels. Tests won't run if the necessary conditions are not met.

--- tests/test_io/test_meshes/test_read_topography.py
+++ tests/test_io/test_meshes/test_read_topography.py
@@ -1,9 +1,16 @@
+import pytest
+
+from conftest import RequirementsLevel
 from subsurface import TriSurf, StructuredGrid
 from subsurface.reader import read_structured_topography, read_unstructured_topography
 from subsurface.structs.base_structures.common_data_utils import replace_outliers
 from subsurface.visualization import to_pyvista_mesh, pv_plot, to_pyvista_grid
 
 
+@pytest.mark.skipif(
+    condition=(RequirementsLevel.MESH | RequirementsLevel.PLOT) not in RequirementsLevel.REQUIREMENT_LEVEL_TO_TEST(),
+    reason="Need to set the READ_WELL variable to run this test"
+)
 def test_read_topography_from_dxf(data_path):
     topo_path = data_path + '/topo/Topografia.dxf'
     unstruct = read_unstructured_topography(topo_path)
@@ -12,6 +19,11 @@ def test_read_topography_from_dxf(data_path):
     pv_plot([s], image_2d=True)
 
 
+
+@pytest.mark.skipif(
+    condition=(RequirementsLevel.READ_GEOSPATIAL) not in RequirementsLevel.REQUIREMENT_LEVEL_TO_TEST(),
+    reason="Need to set the READ_WELL variable to run this test"
+)
 def test_read_topography_from_tif(data_path):
     """
     Notes:
