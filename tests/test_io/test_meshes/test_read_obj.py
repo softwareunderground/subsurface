fca5a5d9cef25b6b09393bee616d38b2276b5f7d
MigueldelaVarga <miguel@terranigma-solutions.com>
2024-04-26 09:39:11 +0200

    [TEST] Run read_obj

--- tests/test_io/test_meshes/test_read_obj.py
+++ tests/test_io/test_meshes/test_read_obj.py
@@ -1,12 +1,18 @@
-﻿import pyvista as pv
+﻿import pytest
+import pyvista as pv
 from dotenv import dotenv_values
 
+from conftest import RequirementsLevel
 
 config = dotenv_values()
 path_to_obj = config.get('PATH_TO_OBJ')
 path_to_mtl = config.get('PATH_TO_MTL')
 
 
+@pytest.mark.skipif(
+    condition=(RequirementsLevel.MESH | RequirementsLevel.PLOT) not in RequirementsLevel.REQUIREMENT_LEVEL_TO_TEST(),
+    reason="Need to set the READ_MESH variable to run this test"
+)
 def test_read_obj():
     reader = pv.get_reader(path_to_obj)
     mesh = reader.read()
