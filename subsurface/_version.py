29dd0105445f3744e58aafe5b135725cc1f3458d
MigueldelaVarga <miguel@terranigma-solutions.com>
2024-06-12 14:59:42 +0200

    [DOC] Fixing all vector docs except the Bayesian part

--- subsurface/_version.py
+++ subsurface/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '2024.1.0'
-__version_tuple__ = version_tuple = (2024, 1, 0)
+__version__ = version = '2024.1.1.dev36+gcb7535a.d20240612'
+__version_tuple__ = version_tuple = (2024, 1, 1, 'dev36', 'gcb7535a.d20240612')
